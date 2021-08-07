effaee11d314d755621f78a323258550a5f5c8ff
Carlos Dzul <c.dzul@hotmail.com>
2021-08-06 21:20:10 -0600

    Funciones

---
+++ Python/Funciones.py
@@ -0,0 +1,23 @@
+
+def demo():
+    num1=5
+    num2=7
+    print(num1+num2)
+
+demo()
+
+
+def demo2(num1, num2):
+	print(num1+num2)
+
+demo2(7,7)
+
+
+def suma3(num1, num2):
+    resultado= num1+num2
+    return resultado
+    
+print(suma3(11,11))  
+
+nuevo_resultado = suma3(44,44)
+print(nuevo_resultado)
